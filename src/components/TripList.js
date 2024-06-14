5c836f2654d7439d04c17eb6e30668506f8223fa
John Knotts <jhnnyk@gmail.com>
2024-06-14 15:09:55 -0600

    the useEffect dependency array

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -3,12 +3,13 @@ import './TripList.css'
 
 export default function TripList() {
   const [trips, setTrips] = useState([])
+  const [url, setUrl] = useState('http://localhost:3000/trips')
 
   useEffect(() => {
-    fetch('http://localhost:3000/trips')
+    fetch(url)
       .then(response => response.json())
       .then(json => setTrips(json))
-  }, [])
+  }, [url])
 
   return (
     <div className="trip-list">
@@ -21,6 +22,15 @@ export default function TripList() {
           </li>
         ))}
       </ul>
+
+      <div className="filters">
+        <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}>
+          European Trips
+        </button>
+        <button onClick={() => setUrl('http://localhost:3000/trips')}>
+          All Trips
+        </button>
+      </div>
     </div>
   )
 }
